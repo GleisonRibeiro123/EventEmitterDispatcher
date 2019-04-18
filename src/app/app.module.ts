d8fbf219e61ae96c0fa0868521eeddd6ec933853
Gleison Ribeiro <gleison.ribeiro@intranet.optcode.com.br>
2019-04-18 17:38:40 -0300

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
+import { RecebeComponent } from './recebe/recebe.component';
+import { Recebe2Component } from './recebe2/recebe2.component';
+import { Recebe3Component } from './recebe3/recebe3.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      TesteComponent,
+      RecebeComponent,
+      Recebe2Component,
+      Recebe3Component
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
